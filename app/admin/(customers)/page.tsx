4eda15f06085f5c0e51cec13251233fa0d64be71
Hojiakbar Habibullayev <hojiakbarhabibullayev@Hojiakbarui-MacBookPro.local>
2025-05-09 23:18:38 +0900

    Admin UI (customers)

---
+++ app/admin/(customers)/page.tsx
@@ -0,0 +1,49 @@
+import Filter from '@/components/shared/filter'
+import { Separator } from '@/components/ui/separator'
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '@/components/ui/table'
+
+const Page = () => {
+	return (
+		<>
+			<div className='flex justify-between items-center w-full'>
+				<h1 className='text-xl font-bold'>Customers</h1>
+				<Filter />
+			</div>
+			<Separator />
+
+			<Table className='text-sm'>
+				<TableCaption>A list of your recent customers.</TableCaption>
+				<TableHeader>
+					<TableRow>
+						<TableHead>#</TableHead>
+						<TableHead>Email</TableHead>
+						<TableHead>Full Name</TableHead>
+						<TableHead>Orders</TableHead>
+						<TableHead>Status</TableHead>
+						<TableHead className='text-right'>Payments</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					<TableRow>
+						<TableCell>1</TableCell>
+						<TableCell>info@sammi.ac</TableCell>
+						<TableCell>Samar Badriddinov</TableCell>
+						<TableCell>12</TableCell>
+						<TableCell>Active</TableCell>
+						<TableCell className='text-right'>1200$</TableCell>
+					</TableRow>
+				</TableBody>
+			</Table>
+		</>
+	)
+}
+
+export default Page
