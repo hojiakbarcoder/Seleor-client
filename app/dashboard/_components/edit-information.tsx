21637738bd73222ab154783398ec38a5c3f9cd36
Hojiakbar Habibullayev <hojiakbarhabibullayev@Hojiakbarui-MacBookPro.local>
2025-05-09 16:30:22 +0900

    Dashboard/edit-information UI

---
+++ app/dashboard/_components/edit-information.tsx
@@ -0,0 +1,79 @@
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from '@/components/ui/accordion'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from '@/components/ui/dialog'
+import { Edit2 } from 'lucide-react'
+import EmailForm from './email.form'
+import FullNameForm from './fullname.form'
+
+const EditInformation = () => {
+	return (
+		<>
+			<div className='w-full h-52 bg-secondary flex justify-center items-center'>
+				<div className='relative'>
+					<Avatar className='size-32'>
+						<AvatarFallback className='bg-primary text-white text-6xl'>
+							CN
+						</AvatarFallback>
+					</Avatar>
+					<Dialog>
+						<DialogTrigger asChild>
+							<Button
+								size={'icon'}
+								variant={'secondary'}
+								className='absolute right-0 bottom-0 rounded-full border border-primary'
+							>
+								<Edit2 />
+							</Button>
+						</DialogTrigger>
+						<DialogContent>
+							<DialogHeader>
+								<DialogTitle />
+							</DialogHeader>
+						</DialogContent>
+					</Dialog>
+				</div>
+			</div>
+
+			<div className='my-3 bg-secondary px-4'>
+				<Accordion type='single' collapsible>
+					<AccordionItem value='item-1'>
+						<AccordionTrigger>
+							<div className='flex flex-col space-y-0'>
+								<h2 className='font-bold'>Full Name</h2>
+								<p className='text-muted-foreground'>Akbar (Ghost)</p>
+							</div>
+						</AccordionTrigger>
+						<AccordionContent className='border-l border-l-primary pl-4'>
+							<FullNameForm />
+						</AccordionContent>
+					</AccordionItem>
+					<AccordionItem value='item-2'>
+						<AccordionTrigger>
+							<div className='flex flex-col space-y-0'>
+								<h2 className='font-bold'>Email</h2>
+								<p className='text-muted-foreground'>example@gmail.com</p>
+							</div>
+						</AccordionTrigger>
+						<AccordionContent className='border-l border-l-primary pl-4'>
+							<EmailForm />
+						</AccordionContent>
+					</AccordionItem>
+				</Accordion>
+			</div>
+		</>
+	)
+}
+
+export default EditInformation
